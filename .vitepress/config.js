910f850e7c486876f534cdbe31bbf72930dd4a26
Alex <keeper.gen@gmail.com>
2023-05-19 13:21:54 +0300

    Feat: Added a logo, changed the link on GitHub icon in navbar, added main categories to mavbar menu (html, css, js)

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,27 +2,31 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "db-web-dev",
-  description: "Web Dev DataBase",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
+   title: "db-web-dev",
+   description: "Web Dev DataBase",
+   themeConfig: {
+      // https://vitepress.dev/reference/default-theme-config
+      logo: 'assets/icons/logo.svg',
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+      nav: [
+         {text: 'Home', link: '/'},
+         {text: 'HTML', link: 'html/'},
+         {text: 'CSS', link: 'css/'},
+         {text: 'JS', link: 'js/'},
+      ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+      sidebar: [
+         {
+            text: 'Examples',
+            items: [
+               {text: 'Markdown Examples', link: '/markdown-examples'},
+               {text: 'Runtime API Examples', link: '/api-examples'}
+            ]
+         }
+      ],
+
+      socialLinks: [
+         {icon: 'github', link: 'https://github.com/KeepGen/db-web-dev'}
+      ]
+   }
 })
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # db-web-dev
-A personal database about Web Development
+A personal Web Development database
+
+---
+## Getting Started
+``` json
+{
+  ...
+  "scripts": {
+    "docs:dev": "vitepress dev docs",
+    "docs:build": "vitepress build docs",
+    "docs:preview": "vitepress preview docs"
+  },
+  ...
+}
+```
+
+---
+### Change log:
+- 2023.05.19 — Added main categories to navbar menu [ HTML, CSS, JS ]
+- 2023.05.19 — Fixed the link for GitHub in navbar menu
+- 2023.05.19 — Add a website logo
+
+### In Progress:
+- Update Feature section on main page
+
+### Plans:
+- Update Hero section on main page
+  - Add a text under main title
+  - Update buttons and links
+- Add a Favicon
+- Tidy up the files: Remove unnecessary files and extra code
+- Update sidebar menu and use real categories and subcategories
+- Review possible additional categories [ Vue, React, TypeScript, Nuxt, Markdown, Command Line ]
+- Add a category or separate page for — "Useful Links / Tools
