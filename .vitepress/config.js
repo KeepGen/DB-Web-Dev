fae53cb31ec8c1f3a4e8ede086fbd56396c9a7f8
Alex <keeper.gen@gmail.com>
2023-05-29 12:00:56 +0300

    Feat: Personal sidebars, add favicon & minor fixes
    - Created personal sidebars menu for each main categories [ HTML, CSS, JS ]
    
    - Add a Favicon
    
    - Rearranged task changelog in `README.md`

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -4,26 +4,48 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
    title: "db-web-dev",
    description: "Web Dev DataBase",
+   head: [
+      ['link', { rel: "shortcut icon", href: "favicon.svg"}],
+   ],
    themeConfig: {
       // https://vitepress.dev/reference/default-theme-config
       logo: 'assets/icons/logo.svg',
 
       nav: [
-         {text: 'Home', link: '/'},
          {text: 'HTML', link: 'html/'},
          {text: 'CSS', link: 'css/'},
          {text: 'JS', link: 'js/'},
       ],
 
-      sidebar: [
-         {
-            text: 'Examples',
-            items: [
-               {text: 'Markdown Examples', link: '/markdown-examples'},
-               {text: 'Runtime API Examples', link: '/api-examples'}
-            ]
-         }
-      ],
+      sidebar: {
+         '/html/': [
+            {
+               text: 'HTML DataBase',
+               items: [
+                  {text: 'Markdown Examples', link: '/markdown-examples'},
+                  {text: 'Runtime API Examples', link: '/api-examples'}
+               ]
+            }
+         ],
+         '/css/': [
+            {
+               text: 'CSS DataBase',
+               items: [
+                  {text: 'Markdown Examples', link: '/markdown-examples'},
+                  {text: 'Runtime API Examples', link: '/api-examples'}
+               ]
+            }
+         ],
+         '/js/': [
+            {
+               text: 'JavaScript DataBase',
+               items: [
+                  {text: 'Markdown Examples', link: '/markdown-examples'},
+                  {text: 'Runtime API Examples', link: '/api-examples'}
+               ]
+            }
+         ],
+      },
 
       socialLinks: [
          {icon: 'github', link: 'https://github.com/KeepGen/db-web-dev'}
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # db-web-dev
+
 A personal Web Development database
 
 ---
+
 ## Getting Started
+
 ``` json
 {
   ...
@@ -16,20 +19,35 @@ A personal Web Development database
 ```
 
 ---
-### Change log:
-- 2023.05.19 — Added main categories to navbar menu [ HTML, CSS, JS ]
-- 2023.05.19 — Fixed the link for GitHub in navbar menu
-- 2023.05.19 — Add a website logo
+## Tasks
 
-### In Progress:
+#### In Progress:
+
+- Add more details for `Getting Started` section in `README.md`
 - Update Feature section on main page
 
-### Plans:
+#### Backlog:
+
 - Update Hero section on main page
-  - Add a text under main title
-  - Update buttons and links
-- Add a Favicon
+   - Add a text under main title
+   - Update buttons and links
 - Tidy up the files: Remove unnecessary files and extra code
 - Update sidebar menu and use real categories and subcategories
 - Review possible additional categories [ Vue, React, TypeScript, Nuxt, Markdown, Command Line ]
 - Add a category or separate page for — "Useful Links / Tools
+
+---
+
+## Change log:
+
+###### 2023-05-29
+
+- Created personal sidebars menu for each main categories [ HTML, CSS, JS ]
+- Add a Favicon
+- Rearranged task changelog in `README.md`
+
+###### 2023-05-19
+
+- Added main categories to navbar menu [ HTML, CSS, JS ]
+- Fixed the link for GitHub in navbar menu
+- Add a website logo
