56cbc3ebc14042d1e8faa824511eee26dcd0ff8d
Alex <keeper.gen@gmail.com>
2023-05-30 16:28:33 +0300

    Add subcategories to JS, add some content, deploy to Vercel
    - Fill with the information `logical operators`
    
    - Add new **JS** subcategories in  `functions`, `if-else`, `logical operators`, `loops`, `objects`
    
    - Add more details for `Getting Started` section in `README.md`
    
    - Add the website to `Vercel`

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,7 +5,7 @@ export default defineConfig({
    title: "db-web-dev",
    description: "Web Dev DataBase",
    head: [
-      ['link', { rel: "shortcut icon", href: "favicon.svg"}],
+      ['link', { rel: "shortcut icon", href: "favicon.ico"}],
    ],
    themeConfig: {
       // https://vitepress.dev/reference/default-theme-config
@@ -40,8 +40,47 @@ export default defineConfig({
             {
                text: 'JavaScript DataBase',
                items: [
-                  {text: 'Markdown Examples', link: '/markdown-examples'},
-                  {text: 'Runtime API Examples', link: '/api-examples'}
+                  {text: 'Логические операторы',
+                     link: '/js/logical/',
+                     collapsed: true,
+                     items: [
+                        {text: 'Краткое содержание', link: '/js/logical/'},
+                        {text: 'Оператор И', link: '/js/logical/and'},
+                        {text: 'Оператор ИЛИ', link: '/js/logical/or'},
+                        {text: 'Оператор НЕ', link: '/js/logical/not'},
+                     ]
+                  },
+                  {text: 'Условие if/else',
+                     link: '/js/if-else/',
+                     collapsed: true,
+                     items: [
+                        {text: 'Краткое содержание', link: '/js/if-else/'},
+                     ]
+                  },
+                  {text: 'Циклы',
+                     link: '/js/loops/',
+                     collapsed: true,
+                     items: [
+                        {text: 'Краткое содержание', link: '/js/loops/'},
+                        {text: 'Цикл WHILE', link: '/js/loops/while'},
+                        {text: 'Цикл DO WHILE', link: '/js/loops/do-while'},
+                        {text: 'Цикл FOR', link: '/js/loops/for'},
+                     ]
+                  },
+                  {text: 'Функции',
+                     link: '/js/functions/',
+                     collapsed: true,
+                     items: [
+                        {text: 'Краткое содержание', link: '/js/functions/'},
+                     ]
+                  },
+                  {text: 'Объекты',
+                     link: '/js/objects/',
+                     collapsed: true,
+                     items: [
+                        {text: 'Краткое содержание', link: '/js/objects/'},
+                     ]
+                  },
                ]
             }
          ],
@@ -49,6 +88,11 @@ export default defineConfig({
 
       socialLinks: [
          {icon: 'github', link: 'https://github.com/KeepGen/db-web-dev'}
-      ]
+      ],
+
+      footer: {
+         message: 'Released under the MIT License.',
+         copyright: 'Copyright © 2023-present DB-WEB-DEV'
+      }
    }
 })
--- README.md
+++ README.md
@@ -6,24 +6,15 @@ A personal Web Development database
 
 ## Getting Started
 
-``` json
-{
-  ...
-  "scripts": {
-    "docs:dev": "vitepress dev docs",
-    "docs:build": "vitepress build docs",
-    "docs:preview": "vitepress preview docs"
-  },
-  ...
-}
-```
+- Run `npm run docs:dev` to start a local **DEV** server with instant hot updates. The dev server should be running at `http://localhost:5173`. Visit the URL in your browser to see your new site in action!
+
+- To explore the features provided by the default documentation theme, check out the  [Default Theme Config Reference ](https://vitepress.dev/reference/default-theme-config)
 
 ---
 ## Tasks
 
 #### In Progress:
 
-- Add more details for `Getting Started` section in `README.md`
 - Update Feature section on main page
 
 #### Backlog:
@@ -31,23 +22,32 @@ A personal Web Development database
 - Update Hero section on main page
    - Add a text under main title
    - Update buttons and links
-- Tidy up the files: Remove unnecessary files and extra code
+- Tidy up the files: Remove unnecessary files and useless code
 - Update sidebar menu and use real categories and subcategories
-- Review possible additional categories [ Vue, React, TypeScript, Nuxt, Markdown, Command Line ]
 - Add a category or separate page for — "Useful Links / Tools
+- Review possible additional categories [ Vue, React, TypeScript, Nuxt, Markdown, Command Line ]
 
 ---
 
 ## Change log:
 
+###### 2023-05-30
+
+- Fill with the information `logical operators`
+- Add new **JS** subcategories in  `functions`, `if-else`, `logical operators`, `loops`, `objects`
+- Add more details for `Getting Started` section in `README.md`
+- Add the website to `Vercel`
+
 ###### 2023-05-29
 
-- Created personal sidebars menu for each main categories [ HTML, CSS, JS ]
+- Add more details for `Getting Started` section in `README.md`
+- Add `logical operators`, `loops`, `functions` and `objects` main and inner pages (with links). Content have to be reviewed and updated
+- Create personal sidebars menu for each main categories [ HTML, CSS, JS ]
 - Add a Favicon
-- Rearranged task changelog in `README.md`
+- Rearrange task changelog in `README.md`
 
 ###### 2023-05-19
 
-- Added main categories to navbar menu [ HTML, CSS, JS ]
-- Fixed the link for GitHub in navbar menu
+- Add main categories to navbar menu [ HTML, CSS, JS ]
+- Fix the link for GitHub in navbar menu
 - Add a website logo
